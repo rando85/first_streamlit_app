0773b2cb8853312e180b8f254ad0156a3d02132a
Daniel Cisneros Medina <webmasterofpuppetz@gmail.com>
2022-08-03 20:23:19 -0500

    header added

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,5 +22,8 @@ streamlit.dataframe(fruits_to_show)
 
 # Display fruityvice api response
 import requests
+#header
+streamlit.header("Fruityvice Fruit Advice!")
+
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)

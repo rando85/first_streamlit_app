121e922fb0d9d5046b767f01ee94f1d2482e1545
Daniel Cisneros Medina <webmasterofpuppetz@gmail.com>
2022-08-08 21:27:00 -0500

    add fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -65,6 +65,7 @@ def insert_row_snowflake(new_fruit):
         return "Thanks for adding " + new_fruit
 
 # allow the end user to add a fruit to the list
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 insert_row_snowflake(add_my_fruit)
 

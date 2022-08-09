7e7a908917b1dd629ef4035138aa78a1c6b2c86b
Daniel Cisneros Medina <webmasterofpuppetz@gmail.com>
2022-08-08 21:03:34 -0500

    put some order in our code

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,8 @@
 import streamlit
+import pandas
+import requests
+import snowflake.connector
+from urllib.error import URLError
 
 streamlit.title("My Parents New Healthy Diner")
 streamlit.header("Breakfast menu")
@@ -9,7 +13,7 @@ streamlit.text("🥑🍞 Avocado toast")
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-import pandas
+
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index("Fruit")
 
@@ -25,7 +29,7 @@ fruit_choice = streamlit.text_input('What fruit would you like information about
 streamlit.write('The user entered ', fruit_choice)
 
 # Display fruityvice api response
-import requests
+
 #header
 streamlit.header("Fruityvice Fruit Advice!")
 
@@ -36,7 +40,7 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # creates a table
 streamlit.dataframe(fruityvice_normalized)
 
-import snowflake.connector
+streamlit.stop()
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
@@ -48,3 +52,5 @@ streamlit.dataframe(my_data_rows)
 # allow the end user to add a fruit to the list
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 streamlit.write('Thanks for adding: ', add_my_fruit)
+
+my_cur.execute("insert into fruit_load_list values ('from streamlit');")
